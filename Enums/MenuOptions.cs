8330363695ed63046007700b8769460abe8b5e87
Wilson Souto <wilsonsouto@outlook.com.br>
2025-02-25 11:45:23 -0300

    Add MenuOptions enum and update project configuration

---
+++ Enums/MenuOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagement.Enums
+{
+    enum MenuOptions
+    {
+        ViewBooks,
+
+        AddBook,
+
+        DeleteBook,
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using LibraryManagement.Enums;
+using Spectre.Console;
+
+namespace LibraryManagement
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuOptions>()
+                    .Title("What do you want to do next? ")
+                    .AddChoices(Enum.GetValues<MenuOptions>()));
+
+                switch (choice)
+                {
+                    case MenuOptions.ViewBooks:
+                        AnsiConsole.MarkupLine("You chose to view books. Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    case MenuOptions.AddBook:
+                        AnsiConsole.MarkupLine("You chose to add a book. Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    case MenuOptions.DeleteBook:
+                        AnsiConsole.MarkupLine("You chose to delete a book. Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                }
+
+                return;
+            }
+        }
+    }
+}
